d0eecddd584d8f2a0b057c0701b046e6c5b9e1eb
firoz <firoz365@gmail.com>
2020-01-26 09:15:45 +0000

    Added header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import { Route, Switch } from 'react-router-dom'
 import ShopPage from './components/shop/shop.component'
+import Hopmepage from './components/header/header-component';
+import Header from './components/header/header-component';
+
 const HatPage = () => {
   return (<div>HAT PAGE</div>)
 }
@@ -11,6 +14,7 @@ const HatPage = () => {
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/hats' component={HatPage} />
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,23 +1,19 @@
-import React  from 'react';
-import './collection-item.style.scss'; 
+import React from 'react';
+import './collection-item.style.scss';
 
 const CollecitonItem = ({ id, name, imageUrl, price }) => (
-<div className='collection-item'>
+    <div className='collection-item'>
         <div className='image'
             style={{
                 backgroundImage: `url(${imageUrl})`
             }}
         />
 
-        <div>
-
-            <div className='colleciton-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
         </div>
     </div>
 )
-    
+
 export default CollecitonItem;
